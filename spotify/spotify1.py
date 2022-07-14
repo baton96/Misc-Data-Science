bc0345f72293fac31a5d3ca6385d63317222a416
baton96 <podolce@op.pl>
2022-07-14 03:16:04 +0200

    Spotify

---
+++ spotify/spotify1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from imblearn.ensemble import BalancedRandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+df = pd.read_csv('spotify1.csv')
+df.genre = df.genre.fillna('')
+df['radio'] = df.song.str.contains('Radio', regex=False)
+# df['has_feat'] = df.song.str.contains('feat.', regex=False)
+
+genres = set()
+for genre in df.genre:
+    genres.update(genre.split(', '))
+genres.discard('')
+for genre in genres:
+    df[genre] = df.genre.str.contains(genre, regex=False)
+
+X, y = df.drop(columns=['artist', 'song', 'genre', 'explicit']), df.explicit
+clf = BalancedRandomForestClassifier(n_jobs=-1, random_state=42)
+score = cross_val_score(clf, X, y, scoring='balanced_accuracy').mean()
+print(f"Balanced accuracy:{score:f}")  # 0.752412
---
+++ spotify/spotify2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from imblearn.ensemble import BalancedRandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+df = pd.read_csv('spotify2.csv')
+df = df[(df['artist type'] == 'Solo') | (df['artist type'] == 'Band/Group')]
+
+df['top genre'] = df['top genre'].fillna('')
+df['radio'] = df.title.str.contains('Radio', regex=False)
+# df['has_feat'] = df.title.str.contains('feat.', regex=False)
+for genre in ['dance', 'hip hop', 'pop']:
+    df[genre] = df['top genre'].str.contains(genre, regex=False)
+
+X, y = df.drop(columns=['title', 'artist', 'top genre', 'added', 'artist type']), df['artist type']
+clf = BalancedRandomForestClassifier(n_jobs=-1, random_state=42)
+score = cross_val_score(clf, X, y, scoring='balanced_accuracy').mean()
+print(f"Balanced accuracy:{score:f}")  # 0.606238
+
