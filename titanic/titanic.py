62b70d0a9647bbe305c71737a4c7f90a143901ad
baton96 <podolce@op.pl>
2023-03-16 10:45:20 +0000

    Titanic

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,76 +1,69 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
-from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
+X = pd.read_csv("train.csv")
+y = X['Survived']
 
-def test_n_save(name, model):
-    pipeline = make_pipeline(SimpleImputer(), StandardScaler(), model)
-    pipeline.fit(train, y)
-    scores = cross_val_score(pipeline, train, y, cv=KFold(20))
-    print(name, "Cross-validation accuracy:%f" % scores.mean())
+X['Family_Size'] = X['Parch'] + X['SibSp']
+X['Sex'] = X['Sex'] == 'male'
 
-    predictions = pipeline.predict(test)
-    acc = sum(s == p for s, p in zip(survived, predictions)) / len(predictions)
-    print(name, "Real accuracy: %f" % acc)
-
-
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
-data = pd.concat([train, test])
-testId = test.PassengerId
-y = train['Survived']
-
-test_data_with_labels = pd.read_csv('titanic.csv')[['survived', 'ticket', 'name']]
-test_data_with_labels.name = test_data_with_labels.name.apply(lambda name: name.replace('"', ''))
-test.Name = test.Name.apply(lambda name: name.replace('"', ''))
-survived = pd.merge(
-    test, test_data_with_labels,
-    left_on=['Ticket', 'Name'],
-    right_on=['ticket', 'name'],
-    how='left'
-)['survived'].values
-
-data['Family_Size'] = data['Parch'] + data['SibSp']
-data['Sex'] = data['Sex'] == 'male'
-
-data['Title'] = data['Name'].str.extract(r'([A-Za-z]+)\.')
+X['Title'] = X['Name'].str.extract(r'([A-Za-z]+)\.')
 mapping = {
     'Mlle': 'Miss', 'Major': 'Mr', 'Col': 'Mr', 'Sir': 'Mr', 'Don': 'Mr', 'Mme': 'Miss',
     'Jonkheer': 'Mr', 'Lady': 'Mrs', 'Capt': 'Mr', 'Countess': 'Mrs', 'Ms': 'Miss', 'Dona': 'Mrs'
 }
-data.replace({'Title': mapping}, inplace=True)
-for title in list(data['Title'].unique()):
-    mean_age_per_title = data.groupby('Title')['Age'].mean()[title]
-    idxs = (data['Age'].isnull()) & (data['Title'] == title)
-    data.loc[idxs, 'Age'] = mean_age_per_title
-data.loc[data['Fare'].isnull(), 'Fare'] = data['Fare'].mean()
-data['FareBin'] = pd.qcut(data['Fare'], 5).cat.codes
-data['AgeBin'] = pd.qcut(data['Age'], 4).cat.codes
+X.replace({'Title': mapping}, inplace=True)
+for title in list(X['Title'].unique()):
+    mean_age_per_title = X.groupby('Title')['Age'].mean()[title]
+    idxs = (X['Age'].isnull()) & (X['Title'] == title)
+    X.loc[idxs, 'Age'] = mean_age_per_title
+X.loc[X['Fare'].isnull(), 'Fare'] = X['Fare'].mean()
+X['FareBin'] = pd.qcut(X['Fare'], 5).cat.codes
+X['AgeBin'] = pd.qcut(X['Age'], 4).cat.codes
 
-data['Last_Name'] = data['Name'].apply(lambda x: str.split(x, ",")[0])
-data['Family_Survival'] = 0.5
+X['Last_Name'] = X['Name'].apply(lambda x: str.split(x, ",")[0])
+X['Family_Survival'] = 0.5
 
-for _, group in data.groupby(['Last_Name', 'Fare']):
+for _, group in X.groupby(['Last_Name', 'Fare']):
     if len(group) <= 1:
         continue
     family_survival = int(group['Survived'].mean() > 0.5)
-    data.loc[group.index, 'Family_Survival'] = family_survival
+    X.loc[group.index, 'Family_Survival'] = family_survival
 
-for _, group in data.groupby('Ticket'):
+for _, group in X.groupby('Ticket'):
     if len(group) <= 1:
         continue
     for idx, row in group.iterrows():
         if row['Family_Survival'] > 0.5:
             continue
         family_survival = int(group.drop(idx)['Survived'].mean() > 0.5)
-        idxs = data['PassengerId'] == row['PassengerId']
-        data.loc[idxs, 'Family_Survival'] = family_survival
+        idxs = X['PassengerId'] == row['PassengerId']
+        X.loc[idxs, 'Family_Survival'] = family_survival
 
-data = data[['Pclass', 'Sex', 'Family_Size', 'Family_Survival', 'FareBin', 'AgeBin']]
-train = pd.DataFrame()
-test = pd.DataFrame()
-for col in data.columns:
-    train[col] = data[col][:891]
-    test[col] = data[col][891:]
+X = X[['Pclass', 'Sex', 'Family_Size', 'Family_Survival', 'FareBin', 'AgeBin']]
+
+pipeline = make_pipeline(
+    SimpleImputer(), 
+    StandardScaler(), 
+    RandomForestClassifier(n_jobs=-1, random_state=1),
+    # NeighborhoodComponentsAnalysis(n_components=2, random_state=0),
+)
+'''
+X2 = pipeline.fit_transform(X, y)
+plt.scatter(X2[:, 0], X2[:, 1], c=y)
+plt.show()
+'''
+print(
+    "Cross-validation accuracy:%f" % cross_val_score(
+        pipeline,
+        X,
+        y,
+        scoring='balanced_accuracy',
+        cv=StratifiedShuffleSplit(random_state=0)
+    ).mean()
+)
+
